17b9f639f7ad1cf9f5a5233ecbb223dc386de66e
nasstya-st <nastasapervaa@gmail.com>
2023-09-22 18:16:29 +0700

    m2 ex10 looks like working

--- module2/ex10/text_to_cmd_vel/setup.py
+++ module2/ex10/text_to_cmd_vel/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        'listener = py_pubsub.subscriber_member_function:main',
+	'listener = text_to_cmd_vel.subscriber_member_function:main',
         ],
     },
 )
